06c9ada34967a357c3bb26a156a70fcdbb7bf220
Martônio Júnior <martoniojunior@hotmail.com>
2022-12-27 21:58:33 -0300

    added(Gadgets): New Temporary Object component

---
+++ Runtime/Gadgets/TemporaryObject.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using MartonioJunior.Core;
+
+namespace MartonioJunior.Flow
+{
+    [DisallowMultipleComponent]
+    public class TemporaryObject: MonoBehaviour
+    {
+        #region Variables
+        [SerializeField, Min(0f)] float timeToLive;
+        Timer timer;
+        #endregion
+        #region MonoBehaviour Lifecycle
+        void Awake()
+        {
+            timer = Timer.Once(timeToLive, false, onComplete: Destroy);
+            timer.Resume();
+        }
+
+        void Update()
+        {
+            timer.Tick();
+        }
+        #endregion
+        #region Methods
+        private void Destroy(Timer timer)
+        {
+            gameObject.Destroy();
+        }
+        #endregion
+    }
+}
