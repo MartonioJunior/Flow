d69838b38688982c69925f220c78204cfc93385b
Martônio Júnior <martoniojunior@hotmail.com>
2023-03-24 10:59:12 -0300

    change(TimeUtility): Generates functions instead of returning values

--- Runtime/Core/TimeUtility.cs
+++ Runtime/Core/TimeUtility.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace MartonioJunior.Flow
@@ -5,41 +6,41 @@ namespace MartonioJunior.Flow
     public static class TimeUtility
     {
         #region Static Methods
-        public static float GetDeltaTime(bool affectedByTimescale = true)
+        public static Func<float> SourceDeltaTime(bool affectedByTimescale = true)
         {
-            return affectedByTimescale ? Time.deltaTime : Time.unscaledDeltaTime;
+            return affectedByTimescale ? () => Time.deltaTime : () => Time.unscaledDeltaTime;
         }
 
-        public static float GetFixedDeltaTime(bool affectedByTimescale = true)
+        public static Func<float> SourceFixedDeltaTime(bool affectedByTimescale = true)
         {
-            return affectedByTimescale ? Time.fixedDeltaTime : Time.fixedUnscaledDeltaTime;
+            return affectedByTimescale ? () => Time.fixedDeltaTime : () => Time.fixedUnscaledDeltaTime;
         }
 
-        public static float GetGlobalTime(bool affectedByTimescale = true, bool useSystemTime = false)
+        public static Func<float> SourceGlobalTime(bool affectedByTimescale = true, bool useSystemTime = false)
         {
             if (useSystemTime) {
-                return Time.realtimeSinceStartup;
+                return () => Time.realtimeSinceStartup;
             } else if (affectedByTimescale) {
-                return Time.time;
+                return () => Time.time;
             } else {
-                return Time.unscaledTime;
+                return () => Time.unscaledTime;
             }
         }
 
-        public static double GetGlobalTimeDouble(bool affectedByTimescale = true, bool useSystemTime = false)
+        public static Func<double> SourceGlobalTimeDouble(bool affectedByTimescale = true, bool useSystemTime = false)
         {
             if (useSystemTime) {
-                return Time.realtimeSinceStartupAsDouble;
+                return () => Time.realtimeSinceStartupAsDouble;
             } else if (affectedByTimescale) {
-                return Time.timeAsDouble;
+                return () => Time.timeAsDouble;
             } else {
-                return Time.unscaledTimeAsDouble;
+                return () => Time.unscaledTimeAsDouble;
             }
         }
 
-        public static float GetTimeSinceLevelLoad()
+        public static Func<float> SourceTimeSinceLevelLoad()
         {
-            return Time.timeSinceLevelLoad;
+            return () => Time.timeSinceLevelLoad;
         }
         #endregion
     }
